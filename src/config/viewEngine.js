7074367cd9b35a371523a2f4d294a9f2160fb318
Huy Thai <huythaia123@gmail.com>
2022-10-13 11:58:16 +0700

    07 static file

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import path from 'path'
 
 const configViewEngine = (app) => {
+    app.use(express.static('./src/public'))
     app.set('view engine', 'ejs')
     app.set('views', './src/views')
 }
